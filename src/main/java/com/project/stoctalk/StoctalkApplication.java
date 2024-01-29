bc74100a33a5ad620246bfcd770fedc2205dda1d
hyocheol An <anhyocheol@naver.com>
2024-01-29 15:20:11 +0900

    feat : project build

---
+++ src/main/java/com/project/stoctalk/StoctalkApplication.java
@@ -0,0 +1,13 @@
+package com.project.stoctalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoctalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoctalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/stoctalk/StoctalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stoctalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoctalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
